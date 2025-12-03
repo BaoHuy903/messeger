59e2bd0f493dee12055532241e245989889dde16
Lê Ngọc Nhất Hòa <charliehoa05@gmail.com>
2025-12-03 15:01:57 +0700

    Update

--- app/src/main/java/com/endterm/messeger/MainActivity.java
+++ app/src/main/java/com/endterm/messeger/MainActivity.java
@@ -1,24 +1,33 @@
 package com.endterm.messeger;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        RecyclerView usersRecyclerView = findViewById(R.id.users_recycler_view);
+        usersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("Name", "ex@gmail.com"));
+        userList.add(new User("Name", "ex@gmail.com"));
+        userList.add(new User("Name", "ex@gmail.com"));
+        userList.add(new User("Name", "ex@gmail.com"));
+
+        UsersAdapter adapter = new UsersAdapter(userList);
+        usersRecyclerView.setAdapter(adapter);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setItemIconTintList(null);
     }
 }
